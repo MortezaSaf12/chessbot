467c3a4859e993c27cf94a2c1a43e0d72271fe15
Morteza Safari <samo23km@student.ju.se>
2026-02-18 11:07:24 +0100

    add material piece values

---
+++ engine/evaluation.js
@@ -0,0 +1,13 @@
+/**
+ * Material piece values (centipawn scale)
+ */
+const PIECE_VALUES = {
+  p: 100,    // Pawn
+  n: 320,    // Knight
+  b: 330,    // Bishop
+  r: 500,    // Rook
+  q: 900,    // Queen
+  k: 20000,  // King
+};
+
+module.exports = { PIECE_VALUES };
